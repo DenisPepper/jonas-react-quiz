02188fef506e48bd46a471896d58d92d7761d189
DenisPepper <thech@yandex.ru>
2023-09-04 20:09:38 +0300

    Создаст эфект с извлечением данных

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import Header from './Header';
 import { Main } from './Main';
 
 export default function App() {
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div className='app'>
       <Header />
