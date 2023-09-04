81f32a3ad2593c90087e185d317034e659f0a2d9
DenisPepper <thech@yandex.ru>
2023-09-04 20:33:50 +0300

    Добавит редьюсер

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import { Main } from './Main';
 
+const Status = {
+  loading: 'loading',
+  error: 'error',
+  ready: 'ready',
+  active: 'active',
+  finished: 'finished',
+};
+
+const Action = {
+  dataRecived: 'dataRecived',
+};
+
+const initialState = { questions: [], status: Status.loading };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case Action.dataRecived:
+      return { ...state, questions: action.payload };
+    default:
+      return state;
+  }
+};
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
     fetch('http://localhost:8000/questions')
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => dispatch({ type: Action.dataRecived, payload: data }))
       .catch((err) => console.error(err));
   }, []);
 
