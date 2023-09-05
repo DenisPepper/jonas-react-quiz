ed6239f2ad2fbf0daefb5a7a91f9495fac189b88
DenisPepper <thech@yandex.ru>
2023-09-05 19:31:11 +0300

    Доработает компонент начального экрана

--- src/start-screen.js
+++ src/start-screen.js
@@ -1,3 +1,9 @@
 export const StartScreen = () => {
-  return <section>Start screen</section>;
+  return (
+    <div className='start'>
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>X question to test your react mastery</h3>
+      <button>Let's start</button>
+    </div>
+  );
 };
