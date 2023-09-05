c923842af19f9296707f8ed0baf4bdbce9b781a2
DenisPepper <thech@yandex.ru>
2023-09-05 20:27:12 +0300

    Подключит компонент для вариантов ответа

--- src/question.js
+++ src/question.js
@@ -1,3 +1,18 @@
+import { Options } from './options';
+
 export const Question = ({ item }) => {
-  return <div>{item.question}</div>;
+  const { question, options, correctOption, points } = item;
+
+  const handleClick = (evt) => {
+    console.log(evt.target.textContent);
+  };
+
+  return (
+    <div>
+      <h4>{question}</h4>
+      <div className='options'>
+        <Options options={options} clickHandler={handleClick} />
+      </div>
+    </div>
+  );
 };
