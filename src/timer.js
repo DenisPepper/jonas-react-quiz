cd43b0c2cbb2cb19b06a07f06ad89cd64881705c
DenisPepper <thech@yandex.ru>
2023-09-07 19:12:39 +0300

    Очистит из очерерди запущенный интервал

--- src/timer.js
+++ src/timer.js
@@ -2,7 +2,8 @@ import { useEffect } from 'react';
 
 export const Timer = ({ tickHandler, timeRemaining }) => {
   useEffect(() => {
-    setInterval(() => tickHandler(), 1000);
+    const interval = setInterval(() => tickHandler(), 1000);
+    return () => clearInterval(interval);
   }, [tickHandler]);
 
   return <div className='timer'>{timeRemaining}</div>;
