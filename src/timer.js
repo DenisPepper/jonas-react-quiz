0bf0d096a9c7eabd69cffcb0f6008234a9e802e2
DenisPepper <thech@yandex.ru>
2023-09-07 19:38:28 +0300

    Отформатирует таймер

--- src/timer.js
+++ src/timer.js
@@ -1,10 +1,19 @@
 import { useEffect } from 'react';
 
-export const Timer = ({ tickHandler, timeRemaining }) => {
+export const Timer = ({ tickHandler, timeRemaining: seconds }) => {
+  const m = Math.floor(seconds / 60);
+  const s = seconds % 60;
+
   useEffect(() => {
     const interval = setInterval(() => tickHandler(), 1000);
     return () => clearInterval(interval);
   }, [tickHandler]);
 
-  return <div className='timer'>{timeRemaining}</div>;
+  return (
+    <div className='timer'>
+      {m < 10 ? '0' : ''}
+      {m}:{s < 10 ? '0' : ''}
+      {s}
+    </div>
+  );
 };
